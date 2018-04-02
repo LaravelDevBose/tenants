a8473ac7dfeaa1393cf8eaeb1d4849eb9fec0a65
LaravelDevBose <laravel.devbose@github.com>
2018-04-03 02:13:10 +0600

    last

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('identify_number')->nullable();
             $table->string('name');
             $table->string('username');
             $table->string('email',100);
