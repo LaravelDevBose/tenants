83eecb0760792103be916fbdc6d5b163bbaa57e0
Larave dev bose <Laravel.devbose@gmail.com>
2021-01-13 21:50:39 -0700

    update index

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,28 +8,28 @@
 
     <!-- Global stylesheets -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
-    <link href="{{asset('public/assets/')}}/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
-    <link href="{{asset('public/assets/')}}/css/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="{{asset('public/assets/')}}/css/core.css" rel="stylesheet" type="text/css">
-    <link href="{{asset('public/assets/')}}/css/components.css" rel="stylesheet" type="text/css">
-    <link href="{{asset('public/assets/')}}/css/colors.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/')}}/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/')}}/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/')}}/css/core.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/')}}/css/components.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/')}}/css/colors.css" rel="stylesheet" type="text/css">
     <!-- /global stylesheets -->
 
     <!-- Core JS files -->
-    <script type="text/javascript" src="{{asset('public/assets/')}}/js/plugins/loaders/pace.min.js"></script>
-    <script type="text/javascript" src="{{asset('public/assets/')}}/js/core/libraries/jquery.min.js"></script>
-    <script type="text/javascript" src="{{asset('public/assets/')}}/js/core/libraries/bootstrap.min.js"></script>
-    <script type="text/javascript" src="{{asset('public/assets/')}}/js/plugins/loaders/blockui.min.js"></script>
+    <script type="text/javascript" src="{{asset('assets/')}}/js/plugins/loaders/pace.min.js"></script>
+    <script type="text/javascript" src="{{asset('assets/')}}/js/core/libraries/jquery.min.js"></script>
+    <script type="text/javascript" src="{{asset('assets/')}}/js/core/libraries/bootstrap.min.js"></script>
+    <script type="text/javascript" src="{{asset('assets/')}}/js/plugins/loaders/blockui.min.js"></script>
     <!-- /core JS files -->
 
     <!-- Theme JS files -->
-    <script type="text/javascript" src="{{asset('public/assets/')}}/js/plugins/forms/validation/validate.min.js"></script>
-    <script type="text/javascript" src="{{asset('public/assets/')}}/js/plugins/forms/styling/uniform.min.js"></script>
+    <script type="text/javascript" src="{{asset('assets/')}}/js/plugins/forms/validation/validate.min.js"></script>
+    <script type="text/javascript" src="{{asset('assets/')}}/js/plugins/forms/styling/uniform.min.js"></script>
 
-    <script type="text/javascript" src="{{asset('public/assets/')}}/js/core/app.js"></script>
-    <script type="text/javascript" src="{{asset('public/assets/')}}/js/pages/login_validation.js"></script>
+    <script type="text/javascript" src="{{asset('assets/')}}/js/core/app.js"></script>
+    <script type="text/javascript" src="{{asset('assets/')}}/js/pages/login_validation.js"></script>
 
-    <script type="text/javascript" src="{{asset('public/assets/')}}/js/plugins/ui/ripple.min.js"></script>
+    <script type="text/javascript" src="{{asset('assets/')}}/js/plugins/ui/ripple.min.js"></script>
     <!-- /theme JS files -->
 
 </head>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,18 +8,18 @@
 
     <!-- Global stylesheets -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
-    <link href="{{asset('public/assets/')}}/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
-    <link href="{{asset('public/assets/')}}/css/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="{{asset('public/assets/')}}/css/core.css" rel="stylesheet" type="text/css">
-    <link href="{{asset('public/assets/')}}/css/components.css" rel="stylesheet" type="text/css">
-    <link href="{{asset('public/assets/')}}/css/colors.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/')}}/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/')}}/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/')}}/css/core.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/')}}/css/components.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('assets/')}}/css/colors.css" rel="stylesheet" type="text/css">
     <!-- /global stylesheets -->
 
     <!-- Core JS files -->
-    <script type="text/javascript" src="{{asset('public/assets/')}}/js/plugins/loaders/pace.min.js"></script>
-    <script type="text/javascript" src="{{asset('public/assets/')}}/js/core/libraries/jquery.min.js"></script>
-    <script type="text/javascript" src="{{asset('public/assets/')}}/js/core/libraries/bootstrap.min.js"></script>
-    <script type="text/javascript" src="{{asset('public/assets/')}}/js/plugins/loaders/blockui.min.js"></script>
+    <script type="text/javascript" src="{{asset('assets/')}}/js/plugins/loaders/pace.min.js"></script>
+    <script type="text/javascript" src="{{asset('assets/')}}/js/core/libraries/jquery.min.js"></script>
+    <script type="text/javascript" src="{{asset('assets/')}}/js/core/libraries/bootstrap.min.js"></script>
+    <script type="text/javascript" src="{{asset('assets/')}}/js/plugins/loaders/blockui.min.js"></script>
     <!-- /core JS files -->
 
     @yield('assets')
